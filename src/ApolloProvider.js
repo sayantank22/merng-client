a8d06c82c3a86bcbd3041043423e32b27ed664fe
Sayantan Karmakar <sayantank22@gmail.com>
2020-09-23 03:52:52 +0530

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://serene-oasis-14928.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
